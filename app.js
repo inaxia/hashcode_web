97682f06031fe11111df548793400fed6ee2bf6a
NISHANT <nishdrummer@gmail.com>
2020-12-04 14:04:49 +0530

    updated the ui

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 //jshint esversion: 6
 
 const express = require("express");
-const bodyParser = require("body-parser");
+const bodyParser =require("body-parser");
+const ejs = require("ejs")
 
 const app= express();
 
 app.use(express.static("public"));
 
-// app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
-// var noOfButtons = document.querySelector
+app.set('view engine', 'ejs');
 
-
-
-
-
-
-
-
-
-
-
-
+const { cpplist, videolinklist } = require('./list.js')
 
 app.get("/", function(req ,res){
-    res.sendFile(__dirname + "/index.html");
-    // res.render("index", {videoList: })
+    // res.sendFile(__dirname + "/index.html");
+    
+    var linksData = [""];
+            
+        
+    res.render("index",{videoListData: cpplist.listData, hrefTag: cpplist.hreftag});
 });
-  
 
-
+app.get("/video",function(req, res){
+    const { cppvideolink } = require('./list.js')
+    res.render("video",{listlink: videolinklist})
+   
+});
 
 app.listen(process.env.PORT || 3000, function(){ //we write process.env.PORT so that heroku defines a port for us
     console.log("Server started on port 3000")
---
+++ list.js
@@ -0,0 +1,15 @@
+var cpplist = {listData: ["Introduction to C++","VS Code installation for C++",
+                "Data Types","Input/Output in C++",
+                "If/else statement in C++","Loops in C++",
+                "Break and Continue statement in C++","Switch Case in C++",
+                "Operators in C++","Awesome Pattern Question-1",
+                "Awesome Pattern Question-2","Awesome Pattern Question-3",
+                "Awesome Pattern Question-4","Awesome Pattern Question-5",
+                "Awesome Pattern Question-6","Awesome Pattern Question-7",
+                "Awesome Pattern Question-8","Awesome Pattern Question-9"],
+                hreftag: "video"};
+var videolinklist = ["https://www.youtube.com/embed/z9bZufPHFLU", 
+                    "https://www.youtube.com/embed/0yn7irrHzM8"]
+module.exports = {cpplist, videolinklist};
+
+
